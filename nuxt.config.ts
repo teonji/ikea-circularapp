2d3752dfe17e7924c0a9753bedeb907cf7ce0631
matteo.tritto <matteo.tritto@weroad.it>
2023-01-02 19:47:00 +0100

    fix zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       title: 'Ikea Circular App',
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
         { name: 'theme-color', content: '#0058AB' },
       ],
     }
