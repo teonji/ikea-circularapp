b59be58adeb77e47616e39857375a794d25d434e
matteo.tritto <matteo.tritto@weroad.it>
2023-01-02 12:41:25 +0100

    fox zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    head: {
+      title: 'Ikea Circular App',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0' },
+        { name: 'theme-color', content: '#0058AB' },
+      ],
+    }
+  },
   modules: ['@nuxtjs/tailwindcss'],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,10 @@
     label: 'Sort by title'
   }, {
     value: 'discount:asc',
-    label: 'Sort by higher discount'
+    label: 'Sort by discount'
   }, {
     value: 'price:asc',
-    label: 'Sort by higher price'
+    label: 'Sort by price'
   }]
 
   const countries = [
@@ -244,7 +244,7 @@
             </div>
           </div>
           <div v-if="!loading" class="md:px-6 mx-4 mb-8">
-            <form class="md:flex items-center mb-4 md:mb-8">
+            <div class="md:flex items-center mb-4 md:mb-8">
               <div class="relative w-full">
                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                   <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
@@ -254,7 +254,7 @@
                   <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
               </div>
-            </form>
+            </div>
             <div class="md:flex justify-between items-center mb-8">
               <div class="md:flex justify-between items-center">
                 <h2 v-if="storeSelected" class="text-gray-500 text-lg">
